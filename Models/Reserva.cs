a86787a9ae6ee720b7a4e3206956646925c9aca3
João Vítor F Martiniano <115907286+JoaoV-Mart@users.noreply.github.com>
2024-09-20 20:12:27 -0300

    Update Reserva.cs

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -17,7 +17,7 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
             // *IMPLEMENTE AQUI*
-            if (Suite.Capacidade >= Hospedes.Count)
+            if (Suite.Capacidade >= hospedes.Count)
             {
                 Hospedes = hospedes;
             }
@@ -26,9 +26,11 @@ namespace DesafioProjetoHospedagem.Models
                 // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
                 // *IMPLEMENTE AQUI*
                 try
-                {
-                    Hospedes == Suite.Capacidade;
-                    return 0;
+                {   
+                    if(Hospedes.Count <= Suite.Capacidade)
+                    {
+                        
+                    }
                 }
                 catch (Exception)
                 {
